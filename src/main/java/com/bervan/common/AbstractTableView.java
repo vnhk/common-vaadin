b93d95f34909cc4be48134a29b980065f5e70f66
berluk12@gmail.com <berluk12@gmail.com>
2024-10-01 16:51:43 +0200

    Deleted NPE fix

--- src/main/java/com/bervan/common/AbstractTableView.java
+++ src/main/java/com/bervan/common/AbstractTableView.java
@@ -18,7 +18,6 @@ import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -111,7 +110,7 @@ public abstract class AbstractTableView<T extends PersistableTableData> extends 
     }
 
     protected Set<T> loadData() {
-        return this.service.load().stream().filter(e -> !e.getDeleted())
+        return this.service.load().stream().filter(e -> e.getDeleted() == null || !e.getDeleted())
                 .collect(Collectors.toSet());
     }
 
