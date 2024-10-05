e6d933ff25b4ce023f5f92a515f178029d64963d
berluk12@gmail.com <berluk12@gmail.com>
2024-10-05 16:49:01 +0200

    Wysiwyg view/edit switch

--- src/main/java/com/bervan/common/WysiwygTextArea.java
+++ src/main/java/com/bervan/common/WysiwygTextArea.java
@@ -1,6 +1,7 @@
 package com.bervan.common;
 
 import com.vaadin.flow.component.ClientCallable;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
@@ -8,6 +9,8 @@ public class WysiwygTextArea extends VerticalLayout implements AutoConfigurableF
     private String value;
     private final String id;
     private Div editorDiv;
+    private boolean viewMode = false;
+    private Button viewEditSwitchButton;
 
     public WysiwygTextArea(String id) {
         this.id = id;
@@ -20,17 +23,26 @@ public class WysiwygTextArea extends VerticalLayout implements AutoConfigurableF
     }
 
     private void configure(String id, String initValue) {
-        //Id in constructor is required to have more than 1 on the same page...
         editorDiv = new Div();
+
         editorDiv.setId(id);
         if (initValue != null) {
             editorDiv.getElement().setProperty("innerHTML", initValue);
         }
 
-        editorDiv.setWidth("100%");
-        editorDiv.setHeight("300px");
+        setStyle(editorDiv);
 
-        add(editorDiv);
+        viewEditSwitchButton = new Button();
+        setViewEditButtonText();
+        executeViewEditModePropertyChange(id);
+
+        viewEditSwitchButton.addClickListener(click -> {
+            viewMode = !viewMode;
+            executeViewEditModePropertyChange(id);
+            setViewEditButtonText();
+        });
+
+        add(viewEditSwitchButton, editorDiv);
 
         getElement().executeJs(
                 "var link = document.createElement('link'); " +
@@ -51,6 +63,27 @@ public class WysiwygTextArea extends VerticalLayout implements AutoConfigurableF
                         "document.body.appendChild(script);",
                 getElement()
         );
+    }
+
+    private void executeViewEditModePropertyChange(String id) {
+        if (viewMode) {
+            getElement().executeJs("document.querySelector('#" + id + " .ql-editor').setAttribute('contenteditable', 'false');");
+        } else {
+            getElement().executeJs("document.querySelector('#" + id + " .ql-editor').setAttribute('contenteditable', 'true');");
+        }
+    }
+
+    private void setViewEditButtonText() {
+        if (viewMode) {
+            viewEditSwitchButton.setText("Switch to Edit Mode");
+        } else {
+            viewEditSwitchButton.setText("Switch to View Mode");
+        }
+    }
+
+    private void setStyle(Div div) {
+        div.setWidth("100%");
+        div.setHeight("300px");
     }
 
     @Override
