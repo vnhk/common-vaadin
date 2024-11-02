fdb77ac3a89224bc06a55cc04cd13c9ca8ca422f
berluk12@gmail.com <berluk12@gmail.com>
2024-11-02 16:28:26 +0100

    Sort tests

---
+++ src/main/java/com/bervan/common/model/UtilsMessage.java
@@ -0,0 +1,8 @@
+package com.bervan.common.model;
+
+public class UtilsMessage {
+    public String message;
+    public boolean isSuccess;
+    public boolean isError;
+    public boolean isWarning;
+}
