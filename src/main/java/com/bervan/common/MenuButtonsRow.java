36c38cd883c64e7408745f90545f48d24bfc5a09
berluk12@gmail.com <berluk12@gmail.com>
2024-11-06 19:07:31 +0100

    Styling

---
+++ src/main/java/com/bervan/common/MenuButtonsRow.java
@@ -0,0 +1,8 @@
+package com.bervan.common;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+
+@Tag("navigation-buttons")
+public class MenuButtonsRow extends HorizontalLayout {
+}
