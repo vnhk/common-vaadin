7516067601b9db826e72faf69777b98a93d08184
berluk12@gmail.com <berluk12@gmail.com>
2024-12-25 15:08:15 +0100

    Navigation menu addButtonIfVisible

--- src/main/java/com/bervan/common/MenuNavigationComponent.java
+++ src/main/java/com/bervan/common/MenuNavigationComponent.java
@@ -5,15 +5,21 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class MenuNavigationComponent extends VerticalLayout {
     protected MenuButtonsRow menuButtonsRow = new MenuButtonsRow();
     protected final Map<String, Button> buttons = new HashMap<>();
-    protected final String CURRENT_ROUTE_NAME;
+    protected String CURRENT_ROUTE_NAME;
+    protected String[] notVisibleButtonsRoutes;
 
     public MenuNavigationComponent(String currentRouteName) {
+        init(currentRouteName);
+    }
+
+    private void init(String currentRouteName) {
         CURRENT_ROUTE_NAME = currentRouteName;
         setWidth("0px");
         setHeight("0px");
@@ -25,21 +31,26 @@ public abstract class MenuNavigationComponent extends VerticalLayout {
         );
     }
 
-    public void addButton(HorizontalLayout menuRow, String routeName, String buttonText) {
-        Button button = new Button(buttonText);
-        button.addClickListener(buttonClickEvent ->
-                button.getUI().ifPresent(ui -> ui.navigate(routeName)));
-
-        buttons.put(routeName, button);
-
-        button.addClassName("option-button");
-
-        if (routeName.equals(CURRENT_ROUTE_NAME)) {
-            button.addClassName("selected-route-button");
-        }
-
-        menuRow.add(button);
+    public MenuNavigationComponent(String routeName, String[] notVisibleButtonsRoutes) {
+        init(routeName);
+        this.notVisibleButtonsRoutes = notVisibleButtonsRoutes;
     }
 
+    public void addButtonIfVisible(HorizontalLayout menuRow, String routeName, String buttonText) {
+        boolean inNotVisible = notVisibleButtonsRoutes != null && Arrays.asList(notVisibleButtonsRoutes).contains(routeName);
+        if (notVisibleButtonsRoutes == null || !inNotVisible) {
+            Button button = new Button(buttonText);
+            button.addClickListener(buttonClickEvent ->
+                    button.getUI().ifPresent(ui -> ui.navigate(routeName)));
 
+            buttons.put(routeName, button);
+
+            button.addClassName("option-button");
+
+            if (routeName.equals(CURRENT_ROUTE_NAME)) {
+                button.addClassName("selected-route-button");
+            }
+            menuRow.add(button);
+        }
+    }
 }
