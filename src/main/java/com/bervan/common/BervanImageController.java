0b103702dd7354fe290139f193ef99ddc4ee89f6
berluk12@gmail.com <berluk12@gmail.com>
2025-04-08 14:43:45 +0200

    Images as base64 as default

--- src/main/java/com/bervan/common/BervanImageController.java
+++ src/main/java/com/bervan/common/BervanImageController.java
@@ -2,12 +2,7 @@ package com.bervan.common;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Base64;
 import java.util.List;
-import java.util.Optional;
 
 
 public class BervanImageController extends VerticalLayout implements AutoConfigurableField<List<String>> {
@@ -20,31 +15,9 @@ public class BervanImageController extends VerticalLayout implements AutoConfigu
 
         BervanButton deleteCurrent = new BervanButton("Delete current", event -> viewer.removeCurrent());
         BervanButton addNew = new BervanButton("Add new image TBD");
-        BervanButton storeImageAsBase = new BervanButton("Store image as base64", event -> convertImageToBase64());
 
-        add(viewer, storeImageAsBase, deleteCurrent, addNew);
+        add(viewer, deleteCurrent, addNew);
     }
-
-    private void convertImageToBase64() {
-        Optional<String> img = viewer.getCurrent();
-        if (img.isPresent() && img.get().startsWith("http")) {
-            try (InputStream inputStream = new URL(img.get()).openStream();
-                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-
-                byte[] buffer = new byte[8192];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-
-                byte[] imageBytes = outputStream.toByteArray();
-                viewer.updateCurrent(Base64.getEncoder().encodeToString(imageBytes));
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
 
     @Override
     public List<String> getValue() {
