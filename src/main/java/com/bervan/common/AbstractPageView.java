a08496e6d9ebab5813eee72ac6d24d6961e4a9b8
berluk12@gmail.com <berluk12@gmail.com>
2025-05-10 11:19:15 +0200

    Products, discounts

--- src/main/java/com/bervan/common/AbstractPageView.java
+++ src/main/java/com/bervan/common/AbstractPageView.java
@@ -1,5 +1,6 @@
 package com.bervan.common;
 
+import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.icon.Icon;
@@ -9,6 +10,10 @@ import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.QueryParameters;
+import io.micrometer.common.util.StringUtils;
+
+import java.util.List;
 
 public abstract class AbstractPageView extends VerticalLayout {
     public AbstractPageView() {
@@ -45,4 +50,52 @@ public abstract class AbstractPageView extends VerticalLayout {
         headerLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
         return headerLayout;
     }
+
+    protected boolean updateField(String fieldValue, AbstractField field, boolean atLeastOneParameter) {
+        if (fieldValue != null && !fieldValue.equals("null")) {
+            field.setValue(fieldValue);
+            atLeastOneParameter = true;
+        }
+        return atLeastOneParameter;
+    }
+
+    protected boolean updateFieldWithDefault(Object fieldValue, AbstractField field, boolean atLeastOneParameter, Object defaultVal) {
+        if (fieldValue != null && !fieldValue.toString().equals("null")) {
+            field.setValue(fieldValue);
+            atLeastOneParameter = true;
+        } else {
+            field.setValue(defaultVal);
+        }
+        return atLeastOneParameter;
+    }
+
+    protected Double getDoubleParam(QueryParameters queryParameters, String name) {
+        String singleParam = getSingleParam(queryParameters, name);
+        if (singleParam == null) {
+            return null;
+        }
+        return Double.valueOf(singleParam);
+    }
+
+    protected Integer getIntegerParam(QueryParameters queryParameters, String name) {
+        String singleParam = getSingleParam(queryParameters, name);
+        if (singleParam == null) {
+            return null;
+        }
+        return Integer.valueOf(singleParam);
+    }
+
+    protected static String getString(String shop) {
+        if (shop != null && StringUtils.isBlank(shop.trim())) {
+            shop = null;
+        }
+        return shop;
+    }
+
+
+    protected String getSingleParam(QueryParameters queryParameters, String name) {
+        List<String> values = queryParameters.getParameters().get(name);
+        return (values != null && !values.isEmpty()) ? values.get(0) : null;
+    }
+
 }
