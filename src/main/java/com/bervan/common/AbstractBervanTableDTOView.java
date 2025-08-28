56eb363e47f71b731f9ea59854f2ad286c145330
berluk12@gmail.com <berluk12@gmail.com>
2025-08-28 21:06:30 +0200

    Add Wallet management functionality for Invest Track app
    Introduced Wallet entities, DTOs, services, and views to enable wallet management within the Invest Track application. Includes table views, transaction handling, and modular layout updates for smoother navigation.

---
+++ src/main/java/com/bervan/common/AbstractBervanTableDTOView.java
@@ -0,0 +1,180 @@
+package com.bervan.common;
+
+import com.bervan.common.model.PersistableTableData;
+import com.bervan.common.model.VaadinBervanColumn;
+import com.bervan.common.service.BaseService;
+import com.bervan.core.model.BaseDTO;
+import com.bervan.core.model.BaseModel;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.core.service.DTOMapper;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import lombok.extern.slf4j.Slf4j;
+import org.vaadin.olli.ClipboardHelper;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public abstract class AbstractBervanTableDTOView<ID extends Serializable, T extends PersistableTableData<ID>, DTO extends BaseDTO<ID>> extends AbstractBervanTableView<ID, T> {
+    private BervanLogger bervanLogger;
+    private Class<DTO> dtoClass;
+
+    public AbstractBervanTableDTOView(MenuNavigationComponent pageLayout, BaseService<ID, T> service, BervanLogger log, Class<T> tClass, Class<DTO> dtoClass) {
+        super(pageLayout, service, log, tClass);
+        this.bervanLogger = log;
+        this.dtoClass = dtoClass;
+    }
+
+    @Override
+    protected void refreshData() {
+        try {
+            this.data.removeAll(this.data);
+            List<T> loadedData = loadData();
+            List<T> converted = new ArrayList<>();
+            DTOMapper dtoMapper = new DTOMapper(bervanLogger, new ArrayList<>());
+            for (T d : loadedData) {
+                converted.add((T) dtoMapper.map(((BaseModel) d), dtoClass));
+            }
+            this.data.addAll(converted);
+            this.grid.getDataProvider().refreshAll();
+        } catch (Exception e) {
+            log.error("Failed to load data", e);
+            showErrorNotification("Failed to load data");
+        }
+    }
+
+    @Override
+    protected void buildOnColumnClickDialogContent(Dialog dialog, VerticalLayout dialogLayout,
+                                                   HorizontalLayout headerLayout, String clickedField, T item) {
+        Field field = null;
+        try {
+            List<Field> declaredFields = getVaadinTableFields();
+
+            Optional<Field> fieldOptional = declaredFields.stream()
+                    .filter(e -> e.getAnnotation(VaadinBervanColumn.class).internalName().equals(clickedField))
+                    .filter(e -> !e.getAnnotation(VaadinBervanColumn.class).inEditForm())
+                    .findFirst();
+
+            Optional<Field> editableField = declaredFields.stream()
+                    .filter(e -> e.getAnnotation(VaadinBervanColumn.class).internalName().equals(clickedField))
+                    .filter(e -> e.getAnnotation(VaadinBervanColumn.class).inEditForm())
+                    .findFirst();
+
+            DTOMapper dtoMapper = new DTOMapper(bervanLogger, new ArrayList<>());
+            if (editableField.isPresent()) {
+                field = editableField.get();
+                AutoConfigurableField componentWithValue = buildComponentForField(field, item);
+                VerticalLayout layoutForField = new VerticalLayout();
+                layoutForField.add((Component) componentWithValue);
+                customFieldInEditLayout(layoutForField, componentWithValue, clickedField, item);
+
+                Button dialogSaveButton = new BervanButton("Save");
+
+                Button deleteButton = new BervanButton("Delete Item");
+                deleteButton.addClassName("option-button-warning");
+
+                ClipboardHelper clipboardHelper = getClipboardHelper(field, item, clickedField, componentWithValue);
+
+                HorizontalLayout buttonsLayout = new HorizontalLayout();
+                buttonsLayout.setWidthFull();
+                buttonsLayout.setJustifyContentMode(JustifyContentMode.BETWEEN);
+
+                buttonsLayout.add(new HorizontalLayout(dialogSaveButton, clipboardHelper), deleteButton);
+
+                T finalItem = (T) dtoMapper.map(((BaseDTO) item));
+                deleteButton.addClickListener(buttonClickEvent -> {
+                    modalDeleteItem(dialog, finalItem);
+                });
+
+                Field finalField = tClass.getDeclaredField(field.getName());
+                AutoConfigurableField finalComponentWithValue = componentWithValue;
+                dialogSaveButton.addClickListener(buttonClickEvent -> {
+                    try {
+                        finalField.setAccessible(true);
+                        finalField.set(finalItem, finalComponentWithValue.getValue());
+                        finalField.setAccessible(false);
+
+                        T changed = service.save(finalItem);
+
+                        customPostUpdate(changed);
+
+                    } catch (Exception e) {
+                        log.error("Could not update field value!", e);
+                        showErrorNotification("Could not update value!");
+                    }
+                    dialog.close();
+                });
+
+                dialogLayout.add(headerLayout, layoutForField, buttonsLayout);
+            } else if (fieldOptional.isPresent()) {
+                field = fieldOptional.get();
+                AutoConfigurableField componentWithValue = buildComponentForField(field, item);
+                componentWithValue.setReadOnly(true);
+                VerticalLayout layoutForField = new VerticalLayout();
+                layoutForField.add((Component) componentWithValue);
+                customFieldInEditLayout(layoutForField, componentWithValue, clickedField, item);
+
+                Button deleteButton = new BervanButton("Delete Item");
+                deleteButton.addClassName("option-button-warning");
+
+                ClipboardHelper clipboardHelper = getClipboardHelper(field, item, clickedField, componentWithValue);
+
+                HorizontalLayout buttonsLayout = new HorizontalLayout();
+                buttonsLayout.setWidthFull();
+                buttonsLayout.setJustifyContentMode(JustifyContentMode.BETWEEN);
+
+                buttonsLayout.add(new HorizontalLayout(clipboardHelper), deleteButton);
+
+                T finalItem2 = item;
+                deleteButton.addClickListener(buttonClickEvent -> {
+                    modalDeleteItem(dialog, finalItem2);
+                });
+
+                dialogLayout.add(headerLayout, layoutForField, buttonsLayout);
+
+            }
+        } catch (Exception e) {
+            log.error("Error during using edit modal. Check columns name or create custom modal!", e);
+            showErrorNotification("Error during using edit modal. Check columns name or create custom modal!");
+        } finally {
+            if (field != null) {
+                field.setAccessible(false);
+            }
+        }
+    }
+
+
+    @Override
+    protected T customizeSavingInCreateForm(T newItem) {
+        try {
+            DTOMapper dtoMapper = new DTOMapper(bervanLogger, new ArrayList<>());
+            return (T) dtoMapper.map(((BaseDTO) item));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    protected List<Field> getVaadinTableFields() {
+        return Arrays.stream(dtoClass.getDeclaredFields())
+                .filter(e -> e.isAnnotationPresent(VaadinBervanColumn.class))
+                .toList();
+    }
+
+    @Override
+    protected Optional<Field> getVaadinTableField(String clickedColumnKey) {
+        return Arrays.stream(dtoClass.getDeclaredFields())
+                .filter(e -> e.isAnnotationPresent(VaadinBervanColumn.class))
+                .filter(e -> e.getAnnotation(VaadinBervanColumn.class).internalName().equals(clickedColumnKey))
+                .findFirst();
+    }
+}
